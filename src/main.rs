a9c6ae39a86f0c5bc3b9ade782f121de9cac849c
RapidInnovation-Rahul <rahulmondal@rapidinnovation.dev>
2022-12-18 18:35:19 +0530

    swagger-ui added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #![allow(unused)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::{
+    error::Error,
+    net::Ipv4Addr,
+};
+use mongodb::bson::{oid::ObjectId};
+use utoipa::{
+    OpenApi,
+   };
+   use utoipa_swagger_ui::SwaggerUi;
+   
 mod db;
 use db::connect_database;
 mod routes;
@@ -10,66 +20,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>  {
     let db = connect_database().await;
     let db = web::Data::new(db);
 
+
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(
+            routes::create,
+            routes::read,
+            routes::update,
+            routes::delete
+        ),
+        components(
+            schemas(routes::Todo, routes::TaskName)
+        ),
+        tags(
+            (name = "todo", description = "Todo management endpoints.")
+        ),
+        
+    )]
+    struct ApiDoc;
+    let openapi = ApiDoc::openapi();
+
     HttpServer::new(move || {
         App::new()
         .app_data(db.clone())
-        .route("/create", web::post().to(create))
-        .route("/read", web::get().to(read))
-        .route("/update/{name}", web::post().to(update))
-        .route("/delete/{name}", web::delete().to(delete))
+        .configure(routes::configure(db.clone()))
+        .service(
+            SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-doc/openapi.json", openapi.clone()),
+        )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((Ipv4Addr::UNSPECIFIED, 8080))?
     .run()
     .await;
 
     Ok(())
 }
 
-
-
-// this is for testing...
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use actix_web::{
-        http::{self, header::ContentType},
-        test,
-    };
-
-    #[actix_web::test]
-    async fn test1() {
-        let task1 = TaskName{
-            name : "task2343".to_string(),
-        };
-        let client = connect_database().await;
-        let create_operation = create(web::Data::new(client), web::Json(task1)).await;
-        assert!(create_operation.status().is_success());
-    }
-
-    #[actix_web::test]
-    async fn test2() {
-        let task1 = TaskName{
-            name : "task1".to_string(),
-        };
-        let client = connect_database().await;
-        let read_operation = read(web::Data::new(client)).await;
-        assert!(read_operation.status().is_success());
-    }
-
-    // #[actix_web::test]
-    // async fn test3() {
-    //     let id = "639dc11d48690460c1dfd532".to_string();
-    //     let client = connect_database().await;
-    //     let app = test::init_service(
-    //         App::new()
-    //             .app_data(web::Data::new(client))
-    //             .route("/delete", web::delete().to(delete)),
-    //     )
-    //     .await;
-    //     let req = test::TestRequest::delete().uri("").to_request();
-    //     let resp = test::call_service(&app, req).await;
-    //     assert!(resp.status().is_success());
-    // }
-
-}
 
--- src/routes.rs
+++ src/routes.rs
@@ -4,15 +4,16 @@ use std::{process, vec};
 use futures::StreamExt;
 use serde::{Serialize, Deserialize};
 use mongodb::bson::{oid::ObjectId};
+use utoipa::ToSchema;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct Todo{
     _id : Option<ObjectId>,
     pub task : String,
     pub done : bool,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize, ToSchema)]
 pub struct TaskName{
     pub name : String,
 }
@@ -21,6 +22,28 @@ const DB_NAME: &str = "Actix-todo";
 const COLL_NAME: &str = "todolist";
 
 
+pub fn configure(client: web::Data<Client>) -> impl FnOnce(&mut web::ServiceConfig) {
+    |config: &mut web::ServiceConfig| {
+        config
+            .app_data(client)
+            .route("/create", web::post().to(create))
+            .route("/read", web::get().to(read))
+            .route("/update/{id}", web::put().to(update))
+            .route("/delete/{id}", web::delete().to(delete));
+    }
+}
+
+#[utoipa::path( 
+    post,
+    path = "/create",
+    responses(
+        (status = 200, description = "Successfully Created new task", body = Todo),
+    ),
+    params(
+        ("info" = TaskName, description = "create new task"),
+    )
+)]
+
 pub async fn create(client: web::Data<Client>, info : web::Json<TaskName>)-> HttpResponse{
     let collection: Collection<Document> = client.database(DB_NAME).collection(COLL_NAME);
     let id = collection.estimated_document_count(None).await.unwrap();
@@ -28,11 +51,20 @@ pub async fn create(client: web::Data<Client>, info : web::Json<TaskName>)-> Htt
         "task": info.name.clone(),
         "done": false,
     };
-    collection.insert_one(doc, None).await;
+    collection.insert_one(&doc, None).await;
 
-    HttpResponse::Ok().body("success")
+    HttpResponse::Ok().json(doc)
 }
 
+
+#[utoipa::path(
+    get,
+    path = "/read",
+    responses(
+        (status = 200, description = "Successfull", body = Vec<Todo>),
+    ),
+    
+)]
 pub async fn read(client: web::Data<Client>)->HttpResponse{
     let collection = client.database(DB_NAME).collection::<Todo>(COLL_NAME);
     let mut cur = collection.find(None, None).await.unwrap();
@@ -43,6 +75,14 @@ pub async fn read(client: web::Data<Client>)->HttpResponse{
     HttpResponse::Ok().json(res)
 }
 
+
+#[utoipa::path(
+    put,
+    path = "/update/{id}",
+    responses(
+        (status = 200, description = "updated Successfully"),
+    ),
+)]
 pub async fn update(client: web::Data<Client>, id : web::Path<String>)-> HttpResponse{
     let collection = client.database(DB_NAME).collection::<Todo>(COLL_NAME);
     let id = id.into_inner();
@@ -51,6 +91,15 @@ pub async fn update(client: web::Data<Client>, id : web::Path<String>)-> HttpRes
     HttpResponse::Ok().finish()
 }
 
+
+#[utoipa::path(
+    delete,
+    path = "/delete/{id}",
+    responses(
+        (status = 200, description = "Deleted Successfully"),
+    ),
+    
+)]
 pub async fn delete(client: web::Data<Client>, id : web::Path<String>)-> HttpResponse{
     let collection = client.database(DB_NAME).collection::<Todo>(COLL_NAME);
     let id = id.into_inner();
